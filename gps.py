3dc0b2f1a5512c70d2c0d06c071a8e243ff864ce
Jerome Noiret <jeromenoiret@free.fr>
2022-04-04 19:28:35 +0200

    Separates reading and writing of the file and adds a control of the GPS coordinates

--- gps.py
+++ gps.py
@@ -1,23 +1,7 @@
-"""
-This script converts (geocode) GPS coordinates into postal addresses.
-It uses the Open Street Map API.
-It requires as input a csv data file with two columns without title:
-   One column with the longitude, one column with the latitude.
-   The separator is the comma.
-   The two columns must be of the same length.
-It returns a file results.csv located in the directory where the script is launched.
-The GPS coordinates must be in the form 50.12345,4.12345
-
-Usage :
-
-pip install --upgrade -r requirements.txt
-python gps.py -- file mon_fichier_de_donnees.csv
-
-
-"""
 import requests
 import csv
 import argparse
+import re
 
 
 parser = argparse.ArgumentParser(
@@ -27,15 +11,30 @@ parser.add_argument('--file', metavar='-F',
 args = parser.parse_args()
 
 
-with open(args.file, 'r') as f:
-    my_reader = csv.reader(f)
-    with open('resultats.csv', 'w') as ft:
-        writer = csv.writer(ft)
+def read_csv(file):
+    coord = []
+    with open(file, 'r') as f:
+        my_reader = csv.reader(f)
         for row in my_reader:
-            row[1] = row[1].lstrip()
-            payload = {'format': "jsonv2",
-                       'lat': row[0], 'lon': row[1], 'zoom': 18, 'addressdetails': 1}
-            resp = requests.get(
-                "https://nominatim.openstreetmap.org/reverse?", params=payload)
-            print(resp.json()['display_name'])
-            writer.writerow([row[0], row[1], resp.json()['display_name']])
+            coord.append([row[0], row[1]])
+    return coord
+
+
+def write_csv(liste):
+    with open('resultats.csv', 'w', newline='') as ft:
+        writer = csv.writer(ft)
+        for row in liste:
+            # check GPS coordinates format
+            if re.match(r'((?:[\+-]?[0-9]*[\.,][0-9]+)|(?:[\+-]?[0-9]+))', row[0]) and re.match(r'((?:[\+-]?[0-9]*[\.,][0-9]+)|(?:[\+-]?[0-9]+))', row[1]):
+                payload = {'format': "jsonv2",
+                           'lat': row[0], 'lon': row[1], 'zoom': 18, 'addressdetails': 1}
+                resp = requests.get(
+                    "https://nominatim.openstreetmap.org/reverse?", params=payload)
+                print(resp.json()['display_name'])
+                writer.writerow([row[0], row[1], resp.json()['display_name']])
+            else:
+                print("Coordonnées GPS invalides")
+
+
+# print(read_csv(args.file)
+write_csv(read_csv(args.file))
